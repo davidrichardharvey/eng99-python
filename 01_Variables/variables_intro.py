8b27995f98b114e6dab9d9cc3ad2b76dde90c132
David Harvey <dharvey@spartaglobal.com>
2021-11-19 10:27:20 +0000

    Turned input age into an integer

--- 01_Variables/variables_intro.py
+++ 01_Variables/variables_intro.py
@@ -6,6 +6,6 @@ print(f"Welcome, {name}")
 # Store their responses
 # Try to capture answers that would be strings, integers and floats
 
-age = input("How old are you?\n")
+age = int(input("How old are you?\n"))
 
 print(f"In 5 years time you will be {age + 5} years old.")
