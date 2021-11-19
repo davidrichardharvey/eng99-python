91335968fc08a0d20afa9e391076d72ec2923f09
David Harvey <dharvey@spartaglobal.com>
2021-11-19 09:55:30 +0000

    Working on variables and input

---
+++ 01_Variables/variables_intro.py
@@ -0,0 +1,11 @@
+name = input("What is your name?\n")
+
+print(f"Welcome, {name}")
+
+# Ask questions of the user
+# Store their responses
+# Try to capture answers that would be strings, integers and floats
+
+age = input("How old are you?\n")
+
+print(f"In 5 years time you will be {age + 5} years old.")
